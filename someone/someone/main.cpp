222790cbda622bbef6d8d313c8d243a1c21c12a8
miaoyunlong <miaoyunlong@bytedance.com>
2021-05-23 15:18:02 +0800

    +initial commit

---
+++ someone/someone/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  someone
+//
+//  Created by bytedance on 2021/5/23.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfs3.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
