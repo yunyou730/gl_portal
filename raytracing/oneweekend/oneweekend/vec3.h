9fb72651292758a51e73e25c42d8740de296b86d
miaoyunlong <miaoyunlong@bytedance.com>
2021-09-07 09:24:30 +0800

    +vec3 template

--- raytracing/oneweekend/oneweekend/vec3.h
+++ raytracing/oneweekend/oneweekend/vec3.h
@@ -3,19 +3,20 @@
 #include <cmath>
 #include <iostream>
 
+template<class T>
 class vec3
 {
 public:
     vec3():e{0,0,0} {}
-    vec3(double e0,double e1,double e2):e{e0,e1,e2}{}
+    vec3(T e0,T e1,T e2):e{e0,e1,e2}{}
     
-    double x() const {return e[0];}
-    double y() const {return e[1];}
-    double z() const {return e[2];}
+    T x() const {return e[0];}
+    T y() const {return e[1];}
+    T z() const {return e[2];}
     
     vec3 operator-() const {return vec3(-e[0],-e[1],-e[2]);}
-    double operator[](int i) const {return e[i];}
-    double& operator[](int i) {return e[i];}
+    T operator[](int i) const {return e[i];}
+    T& operator[](int i) {return e[i];}
     
     vec3& operator+=(const vec3& v)
     {
@@ -25,7 +26,7 @@ public:
         return *this;
     }
     
-    vec3& operator*=(const double t)
+    vec3& operator*=(const T t)
     {
         e[0] *= t;
         e[1] *= t;
@@ -33,22 +34,25 @@ public:
         return *this;
     }
     
-    vec3& operator/=(const double t)
+    vec3& operator/=(const T t)
     {
         *this *= 1.0/t;
         return *this;
     }
     
-    double length_squared() const
+    T length_squared() const
     {
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
     
-    double length() const
+    T length() const
     {
         return sqrt(length_squared());
     }
 
 public:
-    double e[3];
+    T e[3];
 };
+
+using point3 = vec3<double>;
+using color = vec3<double>;
